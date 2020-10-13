29ea755e7da3de5db64bb22852228bbc96805ec1
minamortchev <72209556+minamortchev@users.noreply.github.com>
2020-10-13 15:49:53 +0200

    Update Create_tables.sql

--- Create_tables.sql
+++ Create_tables.sql
@@ -27,9 +27,8 @@ CREATE TABLE Bookings
  DateBookedOn DATE NOT NULL, 
  TimeBookedAt TIME NOT NULL, 
  ResourceID   INTEGER NOT NULL, 
- ResourceType VARCHAR(256) NOT NULL REFERENCES Resources(ResourceType),
- FOREIGN KEY (UserID) REFERENCES User(ID),
- FOREIGN KEY (ResourceID) REFERENCES Resources(ResourceID)
+ ResourceType VARCHAR(256) NOT NULL CHECK(ResourceType IN ('Space','Equipment')),
+ FOREIGN KEY (UserID) REFERENCES User(ID)
 );
 
 
@@ -41,4 +40,23 @@ CREATE TABLE Enrollments
  FOREIGN KEY(ClassID) REFERENCES ClassSchedule(ClassID),
  FOREIGN KEY (UserID) REFERENCES User(ID)
 );
+
+INSERT INTO ClassSchedule VALUES(1,'Yoga','Monday','16:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(2,'Zumba','Monday','17:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(3,'Kickboxing','Monday','18:00:00','Kville',20);
+INSERT INTO ClassSchedule VALUES(4,'Pilates Bar','Tuesday','16:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(5,'Rhythm Cardio + Core','Tuesday','17:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(6,'Outdoor HIIT','Tuesday','18:00:00','Kville',20);
+INSERT INTO ClassSchedule VALUES(7,'Rhythm Strength/ MP','Wednesday','16:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(8,'Power Yoga','Wednesday','17:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(9,'Pilates Burn','Thursday','16:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(10,'Zumba','Thursday','17:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(11,'Outdoor HIIT','Thursday','18:00:00','Kville',20);
+INSERT INTO ClassSchedule VALUES(12,'Hatha Yoga','Friday','16:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(13,'Express HIIT','Friday','17:30:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(14,'Outdoor Yoga','Saturday','10:15:00','Kville',20);
+INSERT INTO ClassSchedule VALUES(15,'Kickboxing','Saturday','12:00:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(16,'Rhythm HIIT','Sunday','11:00:00','Zoom',20);
+INSERT INTO ClassSchedule VALUES(17,'Power Yoga','Sunday','17:00:00','Zoom',20);
+
 
