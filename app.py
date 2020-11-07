a6af6122b225610d6aa4169bbc77395d4b04cfa2
Himanshu Jain <hkj4@dj4.edu>
2020-11-07 07:02:40 -0500

    made the brodie page

--- app.py
+++ app.py
@@ -139,6 +139,12 @@ def internal_error(error):
     return render_template('errors/500.html'), 500
 
 
+@app.route('/background_process_test')
+def background_process_test():
+    print("Hello")
+    return {"date1": True, "Date 2": False, "Date3": False}
+
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
